0299260bcc01e60fcc235b1ebf240869322aa791
Alm4my <almamy@tuta.io>
2023-04-17 15:08:58 +0000

    feature: Scan les ports de 0 a 1024

--- netScan.go
+++ netScan.go
@@ -6,10 +6,18 @@ import (
 )
 
 func main() {
-	_, err := net.Dial("tcp", "google.com:443")
-	if err == nil {
-		fmt.Println("Connexion Établie!")
-	} else {
-		fmt.Println("Nous n'avons pas pu nous connecter au serveur!")
+
+	for i := 0; i <= 1024; i++ {
+		adresse := fmt.Sprintf("inphb.ci:%d", i)
+		conn, err := net.Dial("tcp", adresse)
+		if err == nil {
+			fmt.Println("Le port", i, "de l'adresse", adresse, "est ouvert.")
+			err := conn.Close()
+			if err != nil {
+				fmt.Println("Nous avons eu une erreur en fermant la connection à", adresse)
+				continue
+			}
+		}
+		// nous pouvons avoir un else ici si on veut les ports fermes ou filtrés
 	}
 }

f7d4ca8e431112960dbc92c373424160ec077524
Alm4my <almamy@tuta.io>
2023-04-17 15:59:06 +0000

    feature: Ajoute une routine pour lancer le programme en parallèle
    Il y a un problème de cohérence des résultats avec différents ports et hosts.

--- netScan.go
+++ netScan.go
@@ -3,21 +3,28 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 func main() {
 
-	for i := 0; i <= 1024; i++ {
-		adresse := fmt.Sprintf("inphb.ci:%d", i)
-		conn, err := net.Dial("tcp", adresse)
-		if err == nil {
-			fmt.Println("Le port", i, "de l'adresse", adresse, "est ouvert.")
-			err := conn.Close()
-			if err != nil {
-				fmt.Println("Nous avons eu une erreur en fermant la connection à", adresse)
-				continue
+	var waitGroup sync.WaitGroup
+
+	for i := 0; i <= 65535; i++ {
+		waitGroup.Add(1)
+		go func(j int) {
+			defer waitGroup.Done()
+			adresse := fmt.Sprintf("127.0.0.1:%d", j)
+			conn, err := net.Dial("tcp", adresse)
+			if err == nil {
+				fmt.Println("Le port", j, "de l'adresse", adresse, "est ouvert.")
+				err := conn.Close()
+				if err != nil {
+					fmt.Println("Nous avons eu une erreur en fermant la connection à", adresse)
+				}
 			}
-		}
-		// nous pouvons avoir un else ici si on veut les ports fermes ou filtrés
+			// nous pouvons avoir un else ici si on veut les ports fermes ou filtrés
+		}(i)
 	}
+	waitGroup.Wait()
 }
